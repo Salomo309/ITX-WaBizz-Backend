ea499642c4e98c677e7ceae4bf52ccc5b3b0491b
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-02-29 11:55:00 +0700

    chore: clean up env variable to maintain secret

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
-	"database/sql"
 
 	"itx-wabizz/handlers"
 	"itx-wabizz/middlewares"
@@ -16,6 +15,20 @@ func main() {
 	// Load environment variables
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	// Connect to database
+	db, err := sql.Open("mysql", "root:admin123@tcp(mysql:3306)/itxwabizzdb")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	// Coba lakukan ping ke database
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
 	}
 
 	// Initialize Gin router
@@ -29,23 +42,7 @@ func main() {
 	router.GET("/api", handlers.HelloHandler)
 
 	// Get port from .env and start server
-	fmt.Println()
 	router.Run(GetEnvPortOr("8080"))
-
-	db, err := sql.Open("mysql", "root:admin123@tcp(mysql:3306)/itxwabizzdb")
-
-    if err != nil {
-        panic(err.Error())
-    }
-    defer db.Close()
-
-    // Coba lakukan ping ke database
-    err = db.Ping()
-    if err != nil {
-        panic(err.Error())
-    }
-
-    fmt.Println("Koneksi ke database MySQL berhasil!")
 }
 
 func GetEnvPortOr(port string) string {

8a6f807afe900ab600531d7bf13f2cb640579275
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-03-04 16:08:09 +0700

    User Repository ready refactor: tidy up variable for easier usage feat: user repository ready to access db

---
+++ repositories/repositoryInit.go
@@ -0,0 +1,35 @@
+package repositories
+
+import (
+	"database/sql"
+	"log"
+
+	"itx-wabizz/configs"
+)
+
+var (
+	// Database connection and important repositories that are needed throught app lifetime
+	Db *sql.DB
+	UserRepo *MySQLUserRepository
+)
+
+func InitDatabaseConnection() {
+	var err error
+	Db, err = sql.Open("mysql", configs.MysqlUser+":"+configs.MysqlPassword+"@tcp("+configs.MysqlHost+":3306)/"+configs.MysqlDatabase)
+	if err != nil {
+		log.Fatal("Error connecting to database:", err)
+	}
+
+	// Ping database to check if the connection is valid
+	if err := Db.Ping(); err != nil {
+		log.Fatal("Error pinging database:", err)
+	}
+}
+
+func InitRepositories() {
+	var err error
+	UserRepo, err = NewMySQLUserRepository(Db)
+	if err != nil {
+		log.Fatal("Error initiating repositories")
+	}
+}
---
+++ repositories/userRepository.go
@@ -0,0 +1,63 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"itx-wabizz/models"
+)
+
+// Interface for user repository
+type UserRepository interface {
+	Insert(user *models.User) error
+}
+
+// Implementation of user repository
+type MySQLUserRepository struct {
+	db                    *sql.DB
+	insertStmt            *sql.Stmt
+	getUserByGoogleIDStmt *sql.Stmt
+}
+
+// Function to create new user repository
+func NewMySQLUserRepository(db *sql.DB) (*MySQLUserRepository, error) {
+	insertStmt, err := db.Prepare("INSERT INTO Users (google_id, email, name, picture, admin) VALUES (?, ?, ?, ?, ?)")
+	if err != nil {
+		return nil, err
+	}
+
+	getUserByGoogleIDStmt, err := db.Prepare("SELECT user_id, google_id, email, name, picture, admin FROM Users WHERE google_id = ?")
+	if err != nil {
+		return nil, err
+	}
+
+	return &MySQLUserRepository{
+		db:                    db,
+		insertStmt:            insertStmt,
+		getUserByGoogleIDStmt: getUserByGoogleIDStmt,
+	}, nil
+}
+
+// Function to insert new user into database
+func (repo *MySQLUserRepository) Insert(user *models.User) error {
+	_, err := repo.insertStmt.Exec(user.Google_ID, user.Email, user.Name, user.Picture, user.Admin)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Function to get user based on the Google ID
+func (repo *MySQLUserRepository) GetUserByGoogleID(googleID string) (*models.User, error) {
+	row := repo.getUserByGoogleIDStmt.QueryRow(googleID)
+
+	var user models.User
+	err := row.Scan(&user.User_ID, &user.Google_ID, &user.Email, &user.Name, &user.Picture, &user.Admin)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}

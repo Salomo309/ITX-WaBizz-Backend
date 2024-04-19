eac0ce746eadbec69be8c3af7bc3b0b0a8552fc0
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-04-19 22:38:00 +0700

    fix: some minor bug like null variable and forget to init repo

--- models/chatroom.go
+++ models/chatroom.go
@@ -1,11 +1,11 @@
 package models
 
 type ChatList struct {
-	CustomerName string `json:"customerName"`
-    Timendate    string `json:"timendate"`
-    IsRead       string   `json:"isRead"`
-    StatusRead   string `json:"statusRead"`
-    Content      string `json:"content"`
-	MessageType	 string `json:"messageType"`
-    CountUnread  int    `json:"countUnread"`
+	CustomerName string     `json:"customerName"`
+    Timendate    string     `json:"timendate"`
+    IsRead       *string    `json:"isRead"`
+    StatusRead   *string    `json:"statusRead"`
+    Content      string     `json:"content"`
+	MessageType	 string     `json:"messageType"`
+    CountUnread  int        `json:"countUnread"`
 }

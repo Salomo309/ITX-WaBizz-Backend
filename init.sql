500561506365fd85c9992bbe4c7697d24ea7e442
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-05-23 12:38:45 +0700

    refactor: some env var constant and change init sql a bit

--- init.sql
+++ init.sql
@@ -27,15 +27,17 @@ CREATE TABLE IF NOT EXISTS Chat (
 INSERT INTO Chatroom VALUES (NULL,'08141341134','Budiono');
 INSERT INTO Chatroom VALUES (NULL,'08983517193','Vivi');
 INSERT INTO Chatroom VALUES (NULL,'08183920340','Aldo');
-INSERT INTO Users VALUES ('asep@gmail.com', True, False, "");
+
 INSERT INTO Users VALUES ('gabrielpardosi26@gmail.com', True, True, "");
+INSERT INTO Users VALUES ('asep@gmail.com', True, False, "");
 INSERT INTO Users VALUES ('13521059@std.stei.itb.ac.id', True, False, "");
 INSERT INTO Users VALUES ('13521051@std.stei.itb.ac.id', True, False, "");
 INSERT INTO Users VALUES ('margarethaolivia21@gmail.com', True, False, "");
 INSERT INTO Users VALUES ('bakti@gmail.com', False, False, "");
+
 INSERT INTO Chat VALUES (NULL,'asep@gmail.com',1,'2024-03-07 13:00:00', NULL, 'read', 'Sudah diterima', 'text');
 INSERT INTO Chat VALUES (NULL,NULL,1,'2024-03-07 13:01:00', '0', NULL, 'Ok', 'text');
 INSERT INTO Chat VALUES (NULL,NULL,1,'2024-03-07 13:01:00', '0', NULL, 'Terima kasih', 'text');
 INSERT INTO Chat VALUES (NULL,NULL,2,'2024-03-07 14:23:00', '1', NULL, 'Bisa kirimkan resinya?', 'text');
-INSERT INTO Chat VALUES (NULL,'bakti@gmail.com',2,'2024-03-07 14:24:00', NULL, 'delivered', '/photo', 'photo');
+INSERT INTO Chat VALUES (NULL,'bakti@gmail.com',2,'2024-03-07 14:24:00', NULL, 'delivered', 'Selamat pagi', 'text');
 INSERT INTO Chat VALUES (NULL,NULL,3,'2024-03-01 15:01:00', '1', NULL, 'Halo', 'text');
